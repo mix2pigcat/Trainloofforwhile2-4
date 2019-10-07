e790d9d0e0c60f06678804ac1200c3cfab5679a0
thanhle <mix2pigcat@gmaail.com>
2019-10-07 11:55:23 +0700

    filebai

---
+++ javascript.js
@@ -0,0 +1,10 @@
+for(var i=1;i<=7;i++){
+    for(var j=1;j<=21;j++){
+        if(j==1||j==21||i==1||i==7){
+            document.write('*')
+        }else{
+            document.write('&nbsp;&nbsp');
+        }
+    }document.write('<br/>')
+}
+
